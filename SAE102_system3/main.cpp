27f51c234eb26c405cfdedf7196b7e03c9d21923
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2024-10-20 17:02:05 +0200

    Ajout de commentaire pour aider mon cher camarade Hugo Brest-Lestrade :)

--- SAE102_system3/main.cpp
+++ SAE102_system3/main.cpp
@@ -7,15 +7,18 @@ using namespace std;
 
 
 
-unsigned rng (unsigned max)
+// Premièrement, si tu modifies pas ta variable dans la fonction, met la comme CONSTANTE je t'en SUPPLI
+// Deuxièmement, si t'as une variable d'index, met la de type SIZE_T!! Le prof l'a dit 800 fois (au moins)
+
+unsigned rng (const unsigned max)
 {
     std::srand(chrono::high_resolution_clock::now().time_since_epoch().count());
     return rand() % max;
 }
 
 
-
-void generateRandVotes(vector<string> & candidats, unsigned & voters_cnt, vector<unsigned> & votes_eus)
+// Cette fonction ne doit plus être utilisé car on prend les input de la console pour que ce soit compatible avec test Oracles!!
+void generateRandVotes(const vector<string> & candidats, const unsigned & voters_cnt, vector<unsigned> & votes_eus)
 {
     ofstream votes_file ("votes.txt");  // open votes.txt
     for (size_t i = 0; i < voters_cnt; ++i) {  // for every voter,
@@ -42,9 +45,9 @@ void generateRandVotes(vector<string> & candidats, unsigned & voters_cnt, vector
 }
 
 
-size_t QuiALaMajorite(unsigned & nombreVoteurs, vector<unsigned> & votes_eus)
+size_t QuiALaMajorite(unsigned & nombreVoteurs, vector<unsigned> & votes_eus)  // constante
 {
-    for (unsigned y = 0; y < size(votes_eus); ++y)
+    for (unsigned y = 0; y < size(votes_eus); ++y)  // size_t
     {
         if (votes_eus[y] >= nombreVoteurs/2 )
             return  y;
@@ -54,12 +57,12 @@ size_t QuiALaMajorite(unsigned & nombreVoteurs, vector<unsigned> & votes_eus)
     return false;
 }
 
-size_t quiALeMoinsDeVotes (vector<string> & candidats, vector<unsigned> & vote_eus)
+size_t quiALeMoinsDeVotes (vector<string> & candidats, vector<unsigned> & vote_eus)  // constante
 {
     vector<string> candidats2;
     unsigned tmp = vote_eus[0];
-    unsigned indice_tmp = 0;
-    for (unsigned u = 0; u < size(candidats); ++u)
+    unsigned indice_tmp = 0;  // size_t
+    for (unsigned u = 0; u < size(candidats); ++u)  // size_t
     {
         if (vote_eus[u] < tmp)
         {
@@ -67,7 +70,7 @@ size_t quiALeMoinsDeVotes (vector<string> & candidats, vector<unsigned> & vote_e
             indice_tmp = u;
         }
     }
-    for (unsigned z = 0; z < size(candidats); ++z)
+    for (unsigned z = 0; z < size(candidats); ++z)  // size_t
     {
         if (z == indice_tmp)continue;
         else
@@ -83,16 +86,16 @@ size_t quiALeMoinsDeVotes (vector<string> & candidats, vector<unsigned> & vote_e
 
 void afficherVecteur (const vector<string> & candidats)
 {
-    for (unsigned i = 0; i < size(candidats); ++i )
+    for (unsigned i = 0; i < size(candidats); ++i )  // constante
     {
         cout << candidats[i] << endl ;
     }
 }
 
-vector<unsigned> metAJourLesVotes (vector<string> & candidats)
+vector<unsigned> metAJourLesVotes (vector<string> & candidats)  // constante
 {
     vector<unsigned> vote2;
-    for (unsigned b = 0 ; b < size(candidats)-1 ; ++b)
+    for (unsigned b = 0 ; b < size(candidats)-1 ; ++b)  // size_t
     {
             vote2.push_back(0);
     }
@@ -102,7 +105,7 @@ vector<unsigned> metAJourLesVotes (vector<string> & candidats)
 vector<string> metAJourLesCandidats (unsigned & tmp, vector<string> & candidats)
 {
     vector<string> candidats2;
-    for (unsigned c = 0 ; c < size(candidats) ; ++c)
+    for (unsigned c = 0 ; c < size(candidats) ; ++c)  // size_t
     {
         if (c == tmp)
         {
@@ -168,18 +171,18 @@ int main()
                                 "Sacha Hutte" ,
                                 "Yvan Dulé",
                                 "Huberts Eats",
-                                "Marc Assain"};
+                                "Marc Assain"};  // doit être enlevé et être remplacé par input console
     // oi Hugo, utilise ça pour entrer les candidats avec la console pour que ça marche avec fichier Oracle
     // vector<string> candidats = inputCandidates();
-    vector<unsigned> vote_eus = {0,0,0,0,0,0,0,0} ;
+    vector<unsigned> vote_eus = {0,0,0,0,0,0,0,0} ;  // doit être enlevé et être remplacé par input console
 //  afficherVecteur(candidats);
     unsigned nombreVoteurs = 100 ;
-    generateRandVotes(candidats, nombreVoteurs, vote_eus) ;
-    string nomGagnant ;
-    unsigned tmp;
-    unsigned gagnant = 0;
+    generateRandVotes(candidats, nombreVoteurs, vote_eus) ;  // doit être enlevé et être remplacé par input console
+    string nomGagnant ;  // à quoi sert cette string? dans le message tu peux juste mettre genre candidats[gagnant] comme tu fais avec les votes
+    unsigned tmp;  // size_t
+    unsigned gagnant = 0;  // size_t
     unsigned compteurTour = 0;
-    cout << compteurTour + 1 << "tour ! " << endl << endl ;
+    cout << compteurTour + 1 << "tour ! " << endl << endl ;  // euh... commence le à 1 au pire? Et puis il est pas dans la boucle...?
     while (true)
     {
 
